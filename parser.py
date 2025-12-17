16dfc4717dc6efe209f9d785980975849296258e
Sebastian Himan <shiman@unsa.edu.pe>
2025-12-17 07:34:27 -0500

    Eliminar todos los archivos del repositorio

--- parser.py
+++
@@ -1,9 +0,0 @@
-def parse_input():
-    # Aquí puedes definir las reglas de transición o parsear entradas
-    # Por ejemplo, una lista de transiciones como tuplas (estado actual, símbolo leído, siguiente estado, símbolo escrito, movimiento)
-    transitions = [
-        ("q0", "0", "q0", "0", 'R'),
-        ("q0", "1", "q0", "1", 'R'),
-        ("q0", " ", "q_accept", " ", 'R')
-    ]
-    return transitions
